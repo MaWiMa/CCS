1ae8933a5fb07e34113f09806baf757f140e84c0
MaWiMa <WilliV109@gMail.com>
2014-07-16 17:53:18 +0200

    neue Datei:     en/Changing_Canadian_Schools-008.txt geändert:       flipPages.rb

--- flipPages.rb
+++ flipPages.rb
@@ -1,40 +1,40 @@
 #!/usr/bin/ruby
 # encoding: utf-8
 
-#unless ARGV.length < 1
-#puts "Bitte geben Sie eine Datei an!"
-#puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-#exit
-#end
-
-#unless ARGV.length > 1
-#puts "Bitte geben Sie nicht mehr eine Datei an!"
-#puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-#exit
-#end
-
 thisPage = ARGV[0]
 
-=begin
-lang = "EN"
-here = "-Changing_Canadian_Schools-159.txt"
-base = here[0 .. 26]
-nextPageNumber = here[27..29].to_i+1
-backPageNumber = here[27..29].to_i-1
+if ARGV.length < 1
+puts "Bitte geben Sie eine Datei an!"
+puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
+exit
+elsif ARGV.length > 1
+puts "Bitte geben Sie nur eine Datei an!"
+puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
+exit
+elsif thisPage[2..28] != "-Changing_Canadian_Schools-"
+puts "Das scheint nicht der richtige Dateiname zu sein!"
+puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
+exit
+elsif thisPage.gsub(/[^0-9]/,'').to_i > 329
+puts "Es sollte nur 329 Seiten geben!"
+puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
+exit
 
-nextPage = lang+base+nextPageNumber.to_s
-backPage = lang+base+backPageNumber.to_s
-=end
+end
 
-#thisPage = "file"
 thesePageNames = thisPage.gsub(/[0-9].*/,'')
 thisPageNumber = thisPage.gsub(/[^0-9]/,'').to_i
 nextPageNumber = thisPageNumber+1
+if nextPageNumber > 329
+nextPageNumber = 1
+puts "Die letzte Seite ist erreicht, die nächste Seite ist Seite 1!"
+end 
 backPageNumber = thisPageNumber-1
 
-nextPage = thesePageNames + nextPageNumber.to_s
-backPage = thesePageNames + backPageNumber.to_s 
+nextPage = thesePageNames + "%03d" % nextPageNumber.to_s
+backPage = thesePageNames + "%03d" % backPageNumber.to_s
 
-puts backPage
-puts thisPage
+
+puts thisPageNumber
 puts nextPage
+puts backPage
