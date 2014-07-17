8856d46a66d074cc65e123d0f0d164ec70bdaad3
MaWiMa <WilliV109@gMail.com>
2014-07-17 14:08:30 +0200

    geändert:       .gitignore geändert:       en/EN-Changing_Canadian_Schools-008.txt geändert:       flipPages.rb geändert:       makeWebPages.rb

--- flipPages.rb
+++ flipPages.rb
@@ -1,44 +1,16 @@
 #!/usr/bin/ruby
 # encoding: utf-8
 
-thisPage = ARGV[0]
-
-if ARGV.length < 1
-puts "Bitte geben Sie eine Datei an!"
-puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-exit
-elsif ARGV.length > 1
-puts "Bitte geben Sie nur eine Datei an!"
-puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-exit
-elsif thisPage[2..28] != "-Changing_Canadian_Schools-"
-puts "Das scheint nicht der richtige Dateiname zu sein!"
-puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-exit
-elsif thisPage.gsub(/[^0-9]/,'').to_i > 329
-puts "Es sollte nur 329 Seiten geben!"
-puts "Beispiel: #{__FILE__} EN-Changing_Canadian_Schools-159.txt"
-exit
-
+class Flip
+attr_accessor :lang, :page, :thesePageNames, :thisPage, :nextPageNumber, :backPageNumber, :nextPage, :backPage
+ def initialize(l,p)
+ @lang = l
+ @page = p
+ @thesePageNames = "-Changing_Canadian_Schools-"
+ @thisPage = @lang + @thesePageNames + "%03d" % @page
+ @nextPageNumber = @page.to_i+1
+ @backPageNumber = @page.to_i-1
+ @nextPage = @lang + @thesePageNames + "%03d" % @nextPageNumber.to_s
+ @backPage = @lang + @thesePageNames + "%03d" % @backPageNumber.to_s
+ end
 end
-
-thesePageNames = thisPage.gsub(/[0-9].*/,'')
-thisPageNumber = thisPage.gsub(/[^0-9]/,'').to_i
-nextPageNumber = thisPageNumber+1
-if nextPageNumber > 329
-nextPageNumber = 1
-puts "Die letzte Seite ist erreicht, die nächste Seite ist Seite 1!"
-end 
-backPageNumber = thisPageNumber-1
-if backPageNumber < 1
-backPageNumber = 329
-puts "Die erste Seite ist erreicht, die vorherige Seite ist Seite 329!"
-end 
-
-nextPage = thesePageNames + "%03d" % nextPageNumber.to_s
-backPage = thesePageNames + "%03d" % backPageNumber.to_s
-
-
-puts thisPageNumber
-puts nextPage
-puts backPage
--- makeWebPages.rb
+++ makeWebPages.rb
@@ -1,8 +1,49 @@
 #!/usr/bin/ruby
 # encoding: utf-8
-#require_relative 'flipPages'
+require_relative 'flipPages'
+require 'fileutils'
+require 'asciidoctor'
 
-flipPages.rb("EN-Changing_Canadian_Schools-200.txt")
-#for i in 1..10
-#exec flipPages EN-Changing_Canadian_Schools-159.txt
-#end
+i = "EN"
+a = 8
+flip = Flip.new(i,a)
+
+
+# CCS hat 329 Seiten, die HTML-Seiten sollen durchlaufen
+ if flip.page < 1
+   puts "Die Seitenzahl sollte größer 0 Seiten sein!"
+   exit
+ elsif flip.page > 329
+   puts "Es sollte nur 329 Seiten geben!"
+   exit
+  end
+ if flip.nextPageNumber > 329
+ flip.nextPageNumber = 1
+ puts "Die letzte Seite ist erreicht, die nächste Seite ist Seite 1!"
+ flip.nextPage = flip.lang + flip.thesePageNames + "%03d" % flip.nextPageNumber.to_s
+ end 
+ if flip.backPageNumber < 1
+ flip.backPageNumber = 329
+ puts "Die erste Seite ist erreicht, die vorherige Seite ist Seite 329!"
+ flip.backPage = flip.lang + flip.thesePageNames + flip.backPageNumber.to_s
+ end 
+# backup file
+
+org = flip.lang.downcase+"/"+flip.thisPage+".txt"
+new = "_tmp/"+flip.thisPage+".ad"
+FileUtils.cp(org,new)
+
+f = File.open(new, "a")
+    f.write " \n"
+    f.write "link:index{ext-relative}[home] + \n"
+    f.write "link:/"+flip.lang.downcase+"/"+flip.nextPage+"{ext-relative}[forward] + \n"
+    f.write "link:/"+flip.lang.downcase+"/"+flip.backPage+"{ext-relative}[back] + \n"
+    f.write "link:/copies-from-original/BestCopy_Changing_Canadian_Schools_Perspectives_on_Disability_and_Inclusion.pdf[copy of Changing Canadian Schools] + \n"
+    f.write "[.lead] \n"
+    f.write flip.thisPage+" \n"
+f.close
+
+#Asciidoctor.render_file new, :in_place => true, :safe => 'unsafe'
+#Asciidoctor.render_file new, :to_dir => '_site/en/', :safe => 'unsafe'
+Asciidoctor.render_file new, :to_dir => '_site/'+flip.lang.downcase, :safe => 'unsafe'
+
